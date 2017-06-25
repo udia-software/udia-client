fb600b4007a9701d29d11a31675fb223496652bd
Alexander Wong <admin@alexander-wong.com>
2017-06-25 11:01:09 -0600

    added redux structure for auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
+import rootSaga from './sagas';
+import App from './App';
 import './index.css';
+
+
+const sagaMiddleware = createSagaMiddleware();
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/sagas/index.js
@@ -0,0 +1,174 @@
+import { effects } from 'redux-saga';
+
+import {
+  SENDING_REQUEST,
+  LOGIN_REQUEST,
+  REGISTER_REQUEST,
+  SET_AUTH,
+  SET_USER,
+  LOGOUT,
+  REQUEST_ERROR
+} from '../actions/constants';
+
+import { me, signup, signin, signout } from '../auth';
+
+export function* register({
+  username,
+  password
+}) {
+  yield effects.put({
+    type: SENDING_REQUEST,
+    sending: true
+  });
+
+  try {
+    return yield effects.call(signup, username, password);
+  } catch (exception) {
+    yield effects.put({
+      type: REQUEST_ERROR,
+      error: exception.errors
+    });
+    return false;
+  } finally {
+    yield effects.put({
+      type: SENDING_REQUEST,
+      sending: false
+    });
+  }
+}
+
+export function* login({
+  username,
+  password
+}) {
+  yield effects.put({
+    type: SENDING_REQUEST,
+    sending: true
+  });
+
+  try {
+    return yield effects.call(signin, username, password);
+  } catch (exception) {
+    yield effects.put({
+      type: REQUEST_ERROR,
+      error: exception.error
+    });
+    return false;
+  } finally {
+    yield effects.put({
+      type: SENDING_REQUEST,
+      sending: false
+    });
+  }
+}
+
+/**
+ * Effect to handle logging out
+ */
+export function* logout() {
+  yield effects.put({
+    type: SENDING_REQUEST,
+    sending: true
+  });
+
+  try {
+    const response = yield effects.call(signout);
+    return response;
+  } catch (error) {
+    yield effects.put({
+      type: REQUEST_ERROR,
+      error: error.message
+    });
+    return false;
+  } finally {
+    yield effects.put({
+      type: SENDING_REQUEST,
+      sending: false
+    });
+  }
+}
+
+/**
+ * Log in saga
+ */
+export function* loginFlow() {
+  while (true) {
+    const request = yield effects.take(LOGIN_REQUEST);
+    const {
+      username,
+      password
+    } = request.data;
+
+    const winner = yield effects.race({
+      auth: effects.call(login, {
+        username,
+        password
+      }),
+      logout: effects.take(LOGOUT)
+    });
+
+    if (winner.auth) {
+      yield effects.put({
+        type: SET_AUTH,
+        newAuthState: true
+      });
+      yield effects.put({
+        type: SET_USER,
+        newUserState: me()
+      });
+    }
+  }
+}
+
+/**
+ * Log out saga
+ */
+export function* logoutFlow() {
+  while (true) {
+    yield effects.take(LOGOUT);
+    yield effects.put({
+      type: SET_AUTH,
+      newAuthState: false
+    });
+    yield effects.put({
+      type: SET_USER,
+      newUserState: me()
+    });
+    yield effects.call(logout);
+  }
+}
+
+/**
+ * Register saga
+ */
+export function* registerFlow() {
+  while (true) {
+    const request = yield effects.take(REGISTER_REQUEST);
+    const {
+      username,
+      password
+    } = request.data;
+
+    const wasSuccessful = yield effects.call(register, {
+      username,
+      password
+    });
+
+    if (wasSuccessful) {
+      yield effects.put({
+        type: SET_AUTH,
+        newAuthState: true
+      });
+      yield effects.put({
+        type: SET_USER,
+        newUserState: me()
+      });
+    }
+  }
+}
+
+export default function* root() {
+  yield effects.fork(loginFlow);
+  yield effects.fork(logoutFlow);
+  yield effects.fork(registerFlow);
+}
