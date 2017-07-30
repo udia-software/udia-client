20aa35ff7747d3aafe9addd542be63a35894626a
Alexander Wong <admin@alexander-wong.com>
2017-07-29 23:39:22 -0600

    Added footer, updated Semantic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,31 @@
-import React, { Component } from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import PrivateRoute from './Auth/PrivateRoute';
-import Profile from './Auth/Profile';
-import Signin from './Auth/Signin';
-import Signup from './Auth/Signup';
-import CreatePost from './Posts/CreatePost';
-import PostContainer from './Posts/PostContainer';
-import PostList from './Posts/PostList';
-import User from './Users/User';
-import About from './About';
-import Navbar from './Navbar';
+import PrivateRoute from "./Auth/PrivateRoute";
+import Profile from "./Auth/Profile";
+import Signin from "./Auth/Signin";
+import Signup from "./Auth/Signup";
+import CreatePost from "./Posts/CreatePost";
+import PostContainer from "./Posts/PostContainer";
+import PostList from "./Posts/PostList";
+import User from "./Users/User";
+import About from "./About";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const NoMatch = ({ location }) => (
   <div>
     <h3>Page not found!</h3>
     <p>No match found for <code>{location.pathname}</code></p>
-    <p><Link to='/'>Go to the home page →</Link></p>
+    <p><Link to="/">Go to the home page →</Link></p>
   </div>
 );
 
 NoMatch.propTypes = {
   location: PropTypes.shape({
-    pathname: PropTypes.string.isRequired,
-  }).isRequired,
+    pathname: PropTypes.string.isRequired
+  }).isRequired
 };
 
 class App extends Component {
@@ -32,18 +33,21 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={PostList} />
-          <Route path='/about' component={About} />
-          <Route path='/signin' component={Signin} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/users/:username' component={User} />
-          <Route exact path='/posts' component={PostList} />
-          <PrivateRoute path='/posts/create' component={CreatePost} />
-          <Route path='/posts/:id' component={PostContainer} />
-          <PrivateRoute path='/profile' component={Profile} />
-          <Route component={NoMatch} />
-        </Switch>
+        <div style={{ minHeight: "87vh"}}>
+          <Switch>
+            <Route exact path="/" component={PostList} />
+            <Route path="/about" component={About} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/users/:username" component={User} />
+            <Route exact path="/posts" component={PostList} />
+            <PrivateRoute path="/posts/create" component={CreatePost} />
+            <Route path="/posts/:id" component={PostContainer} />
+            <PrivateRoute path="/profile" component={Profile} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { List, Segment } from "semantic-ui-react";
+
+const Footer = () => {
+  return (
+    <Segment textAlign="center" color="teal">
+      <List bulleted horizontal link>
+        <List.Item as={Link} to="/about">About</List.Item>
+        <List.Item as="a" href="https://github.com/udia-software/udia">
+          Source Code
+        </List.Item>
+        <List.Item as={Link} to="/">Udia</List.Item>
+      </List>
+    </Segment>
+  );
+};
+
+export default Footer;
