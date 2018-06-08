238f5b29ad2fb7c24fb88240d3522051b0547898
Alexander Wong <alex@udia.ca>
2018-06-08 11:40:02 -0600

    added router, index refactoring & test

---
+++ src/index.test.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import render from "./index";
+
+it("renders without crashing", () => {
+  expect.assertions(1);
+  const testRoot = document.createElement("div");
+  expect(testRoot).not.toBeNull();
+  render(App, testRoot);
+  ReactDOM.unmountComponentAtNode(testRoot);
+});
