eb92499e17e4a20c2d1d0c27428e92b3d9a47b21
Alexander Wong <alex@udia.ca>
2018-07-04 14:56:41 -0600

    Force remount ApolloProvider, as prop change is not enough

--- src/index.tsx
+++ src/index.tsx
@@ -5,30 +5,28 @@ import { BrowserRouter } from "react-router-dom";
 // tslint:disable-next-line:no-submodule-imports
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./Components/App";
-import RefreshingApolloProvider from "./Components/Wrapper/RefreshingApolloProvider";
+import RefreshingApolloProviderWrapper from "./Components/Wrapper/RefreshingApolloProvider";
 import ToggleableThemeProvider from "./Components/Wrapper/ToggleableThemeProvider";
 import configureReduxStore from "./Modules/ConfigureReduxStore";
 import register from "./registerServiceWorker";
 
-function render(
-  AppComponent: () => JSX.Element,
-  root: HTMLElement | null
-) {
+function render(AppComponent: () => JSX.Element, root: HTMLElement | null) {
   if (!root) {
     throw new Error("Element `root` does not exist!");
   }
-  const supportsHistory = 'pushState' in window.history;
+  const supportsHistory = "pushState" in window.history;
+  const ApolloEnabledApp = RefreshingApolloProviderWrapper(
+    <ToggleableThemeProvider>
+      <BrowserRouter forceRefresh={!supportsHistory}>
+        <AppComponent />
+      </BrowserRouter>
+    </ToggleableThemeProvider>
+  );
   const { persistor, store } = configureReduxStore();
   ReactDOM.render(
     <ReduxProvider store={store}>
       <PersistGate persistor={persistor}>
-        <RefreshingApolloProvider>
-          <ToggleableThemeProvider>
-            <BrowserRouter forceRefresh={!supportsHistory}>
-              <AppComponent />
-            </BrowserRouter>
-          </ToggleableThemeProvider>
-        </RefreshingApolloProvider>
+        <ApolloEnabledApp />
       </PersistGate>
     </ReduxProvider>,
     root
